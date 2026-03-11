b7b8b2c0393f98b19942665dcfb859e9b99aaea2
eklure7 <105546179+eklure7@users.noreply.github.com>
2026-03-11 23:02:29 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -11,7 +11,7 @@ Running the script will drop the entire 'DataWarehouse' database if it exists.
 All data in the database will be permanently deleted. Proceed with caution and
 ensure you have a proper backup before running the script.
 
-/*
+*/
 
 USE master;
 GO
